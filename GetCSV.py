2e8d6e34a061911bc18a5910b870cad6683bd68d
Keisuke Sakai <penguin22231@gmial.com>
2019-06-19 04:40:30 +0000

    add logger

--- GetCSV.py
+++ GetCSV.py
@@ -7,8 +7,15 @@ import os
 import sys
 import logging
 
+
 def numStr(num:int): -> str
+    '''
+    数字を0埋めして2桁で返す
+    '''
     return str(num).zfill(2)
+
+
+logging.basicConfig(filename='logfile/logger.log', level=logging.DEBUG)
 
 Base = "http://race.sp.netkeiba.com/?pid=race_result&race_id="
 dst = ''
@@ -20,26 +27,31 @@ df_col = ['year', 'date', 'field', 'race', 'race_name'
 if not os.path.exists('./data'):
     os.mkdir('./data')
 
-designated_year = int(sys.argv[1])
+designated_year = 2018
 
 for year in tqdm(range(designated_year, designated_year+1)):
     for i in range(1, 11):
         df = pd.DataFrame()
+        logging.info('DataFrameの作成 完了')
         for j in range(1, 11):
             for k in range(1, 11):
                 for l in range(1, 13):
                     # urlでぶっこ抜く
                     url = Base + str(year) + numStr(i) + numStr(j) + numStr(k) + numStr(l)
                     html = requests.get(url)
+                    logging.info('html取得')
                     html.encoding = 'EUC-JP'
                     
                     # scraping
                     soup = BeautifulSoup(html.text, 'html.parser')
+                    logging.info('parser完了')
                     # ページがあるかの判定
                     if soup.find_all('div', attrs={'class', 'Result_Guide'})!=[]:
+                        logging.info('ページ無し')
                         time.sleep(1)
                         break
                     else:
+                        logging('ページ有り')
                         #共通部分を抜き出す
                         CommonYear = year
                         CommonDate = soup.find_all('div', attrs={'class', 'Change_Btn Day'})[0].string.strip()
@@ -49,6 +61,7 @@ for year in tqdm(range(designated_year, designated_year+1)):
                         CommonCourse= soup.find_all('dd', attrs={'Race_Data'})[0].span.string
                         CommonHcount= soup.find_all('dd', attrs={'class', 'Race_Data'})[0].contents[3].split()[1]
                         
+                        logging.info('共通部分抜き出し 完了')
                         for m in range(len(soup.find_all('div', attrs='Rank'))):
                             dst = pd.Series(index=df_col)
                             try:
@@ -79,10 +92,11 @@ for year in tqdm(range(designated_year, designated_year+1)):
                                 if Odds.dt.string is not None:
                                     dst['odds'] = Odds.dt.string.strip('倍')
                                     dst['popu'] = Odds.dd.string.strip('人気')
-                            except:
-                                time.sleep(1)
+                            
                             dst.name = str(year) + numStr(i) + numStr(j) + numStr(k) + numStr(l) + numStr(m)
-                          
+                          　
                             df = df.append(dst)
+                            logging.info('append 完了')
 
-        df.to_csv('./data/keiba'+ str(year)+str(i) '.csv', encoding='shift-jis')
+        df.to_csv('./data/keiba'+ str(year) + str(i) + '.csv', encoding='shift-jis')
+        logging.info('csv 出力完了')
