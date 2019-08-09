422051b8ba0f80a702b95d2517569f1ce12f6ac8
Keisuke Sakai <penguin22231@gmial.com>
2019-08-09 21:58:28 +0900

    体裁の修正

--- GetCSV.py
+++ GetCSV.py
@@ -8,11 +8,12 @@ import sys
 import logging
 
 
-def numStr(num:int):
+def numStr(num: int):
     '''
     数字を0埋めして2桁で返す
     '''
     return str(num).zfill(2)
+
 
 def scrape_common(soup: BeautifulSoup, designated_year: int):
     """
@@ -22,14 +23,14 @@ def scrape_common(soup: BeautifulSoup, designated_year: int):
     @retun CommonParam:dict
     """
     CommonParam = {}
-    CommonParam["year"] =  designated_year
+    CommonParam["year"] = designated_year
     CommonParam["date"] = soup.find_all('div', attrs={'class', 'Change_Btn Day'})[0].string.strip()
     CommonParam["field"] = soup.find_all('div', attrs={'class', 'Change_Btn Course'})[0].string.strip()
     CommonParam["race"] = soup.find_all('div', attrs={'Race_Num'})[0].span.string
     CommonParam["race_name"] = soup.find_all('dt', attrs={'class', 'Race_Name'})[0].contents[0].strip()
     CommonParam["course"] = soup.find_all('dd', attrs={'Race_Data'})[0].span.string
     CommonParam["head_count"] = soup.find_all('dd', attrs={'class', 'Race_Data'})[0].contents[3].split()[1]
-    
+
     return CommonParam
 
 
@@ -40,11 +41,11 @@ def get_one_record(soup, common):
     @param common: dict
     @return dst: pd.Series
     """
-    dst = pd.Series(index = df_col)
+    dst = pd.Series(index=df_col)
 
     dst['year'] = common['year']
     dst['date'] = common["date"]
-    dst['field']= common['field']
+    dst['field'] = common['field']
     dst['race'] = common['race']
     dst['race_name'] = common['race_name']
     dst['course'] = common['course']
@@ -57,12 +58,12 @@ def get_one_record(soup, common):
     dst['age'] = list(detailL.contents[0].split()[0])[1]
     dst['trainerA'] = detailL.span.string.split('･')[0]
     dst['trainerB'] = detailL.span.string.split('･')[1]
-    if len(detailL.contents[0].split())>=2:
+    if len(detailL.contents[0].split()) >= 2:
         dst['weight'] = detailL.contents[0].split()[1].split('(')[0]
-        if len(detailL.contents[0].split()[1].split('('))>=2:
+        if len(detailL.contents[0].split()[1].split('(')) >= 2:
             dst['c_weight'] = detailL.contents[0].split()[1].split('(')[1].strip(')')
     detailR = soup.find_all('span', attrs=['class', 'Detail_Right'])[m].contents
-    if  "\n" in detailR or "\n▲" in detailR or '\n☆' in detailR:
+    if "\n" in detailR or "\n▲" in detailR or '\n☆' in detailR:
         detailR.pop(0)
     dst['jackie'] = detailR[0].string.strip()
     dst['j_weight'] = detailR[2].strip().replace('(', '').replace(')', '')
@@ -70,7 +71,7 @@ def get_one_record(soup, common):
     if Odds.dt.string is not None:
         dst['odds'] = Odds.dt.string.strip('倍')
         dst['popu'] = Odds.dd.string.strip('人気')
-    
+
     return dst
 
 
@@ -81,15 +82,15 @@ if __name__ == "__main__":
 
     Base = "http://race.sp.netkeiba.com/?pid=race_result&race_id="
     dst = ''
-    df_col = ['year', 'date', 'field', 'race', 'race_name'
-            , 'course', 'head_count', 'rank', 'horse_name'
-            , 'gender', 'age', 'trainerA', 'trainerB', 'weight', 'c_weight', 'jackie', 'j_weight'
-            , 'odds','popu']
+    df_col = ['year', 'date', 'field', 'race', 'race_name',
+              'course', 'head_count', 'rank', 'horse_name',
+              'gender', 'age', 'trainerA', 'trainerB', 'weight', 'c_weight', 'jackie', 'j_weight',
+              'odds', 'popu']
 
     if not os.path.exists('./data'):
         os.mkdir('./data')
 
-    df = pd.DataFrame(index = df_col)
+    df = pd.DataFrame(index=df_col)
     csv_count = 0
     logging.debug('DataFrameの作成 完了')
 
@@ -105,28 +106,28 @@ if __name__ == "__main__":
                     time.sleep(1)
                     logging.debug('html取得')
                     html.encoding = 'EUC-JP'
-                    
+
                     # scraping
                     soup = BeautifulSoup(html.text, 'html.parser')
                     logging.debug('parser完了')
                     # ページがあるかの判定
-                    if soup.find_all('div', attrs={'class', 'Result_Guide'})!=[]:
+                    if soup.find_all('div', attrs={'class', 'Result_Guide'}) != []:
                         logging.debug('ページ無し')
                         break
                     else:
                         logging.debug('ページ有り')
-                        #共通部分を抜き出す
+                        # 共通部分を抜き出す
                         common = scrape_common(soup, designated_year)
                         for m in range(len(soup.find_all('dic', attrs='Rank'))):
                             try:
                                 dst = get_one_record(soup, common)
-                                
+
                                 dst.name = page_id + numStr(m)
                                 df = df.append(dst)
                                 if df.shape[0] >= 5000:
-                                    df.to_csv('./data/keiba' + str(designated_year) + str(csv_count) + '.csv', encoding = 'sjis')
+                                    df.to_csv('./data/keiba' + str(designated_year) + str(csv_count) + '.csv', encoding='sjis')
                                     logging.info('csv No.{} 出力完了'.format(csv_count))
-                                    df = pd.DataFrame(index = df_col)
+                                    df = pd.DataFrame(index=df_col)
                                     csv_count += 1
                             except:
                                 logging.info('レコード取得失敗')
